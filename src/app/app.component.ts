6c42481ee9f5fdfbb8c350b10d3ebbc8b2f5efba
rohitpratapitm <rohitpratapitm@gmail.com>
2020-01-29 14:53:41 -0500

    Added Employee model class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Employee } from './employee/employee.model';
+import employee from '../assets/employee.json';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  templateUrl: './app.component.html'
 })
 export class AppComponent  {
   name = 'Angular';
+  myEmployee: Employee = employee;
 }
